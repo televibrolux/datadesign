4b684b9e0ade418e8e173860e66c882265eebe75
Duane Ulibarri <dulibarri10@cnm.edu>
2016-01-18 23:26:03 -0700

    added more detail

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+	<html>Persona
+		<head>
+	<title>YouTube Persona</title>
+		</head>
+		<body>
+		<p>YouTube Persona</p>
+
+			<p>With over 300 hours of videos uploaded per minute, YouTube has something for everyone.
+
+			While YouTube bills itself as a video venue for the masses, it does not have a clear demographics.
+			People of all ages use YouTube to watch their favorite videos. There is an even split between man and
+			women users. YouTube is now a decade old and one would think that there is a clear demographics
+			for YouTube users. It's an even split between men and women who are YouTube users. There is however
+			a difference who spends more time on YouTube. The 18 to 25 year old YouTube users spend the most time
+			on YouTube. A great deal of advertisers do not target audiences based on their age, instead
+			advertisers target users on what videos are being viewed. Generally men spend their time viewing
+			videos on subjects such as sports and video gaming, while women view videos on subjects such as beauty and makeup.</p>
+
+			<title>Use Cases</title>
+
+			<title>The User:</title>
+
+			<p>The user (The one who uploads videos) can upload any video as long as it adheres to YouTube guidelines.
+			The my be of high quality, HD or low quality (Non HD). The user decides in which catagory to upload
+			their video too. They are endless catagories you can choose from. The user is even allowed to create their
+			own catagory to upload their video too. The user and viewer (The one who only views YouTube videos) can for
+			the most part, comment on the video. There are some videos that block comments but those are far between.</p>
+
+			<title>The Viewer:</title>
+
+			<p>The YouTube viewer can watch any video of their choosing. The viewer typically searches for videos they want to view
+			by entering a name or event into the search bar. The User can also perform this task. I say typically because one can
+			also select videos to watch that are trending and the videos that have gone viral. Those videos are featured on their
+			opening page</p>
+			<head>Relations</head>
+
+			<P>The User:</P>
+			<p> The User can post videos on YouTube that adhere to YouTube guidelines.</p>
+			<p>
+
+			</p>
+
+	</body>
+	</html>
